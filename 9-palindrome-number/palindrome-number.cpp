3d595b350f81d454a83c7bc7a91933617061a5d7
Carlos Eduardo <dudusoutomaior2006@gmail.com>
2025-09-30 19:31:08 -0300

    Time: 1 ms (59.41%) | Memory: 8.6 MB (40.71%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        int xcopy = x;
+        long reverse = 0;
+        while (x>0) {
+            reverse = (reverse * 10) + (x % 10);
+            x /= 10;
+
+        }
+        return reverse == xcopy;
+    }
+};
